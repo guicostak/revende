28bcb8c0b1117e53d689276c7a6694fd9e04b4f1
guicostak <103201399+guicostak@users.noreply.github.com>
2023-03-17 15:50:18 -0300

    estruturando pages com router

---
+++ src/paginas/Cadastro/index.js
@@ -0,0 +1,18 @@
+import Logo from '../../componentes/Logo'
+import Formulario from '../../componentes/Formulario'
+import Rodape from '../../componentes/Rodape'
+import './cadastro.css'
+
+
+const Cadastro = () =>{
+return (
+  <div className="body">
+    <Logo />
+    <Formulario />
+    <Rodape />
+  </div>
+  
+)
+}
+
+export default Cadastro
---
+++ src/paginas/Home/index.js
@@ -0,0 +1,13 @@
+import Header from '../../componentes/Header'
+import './Home.css'
+
+
+const Home = () =>{
+return (
+  <div className="body">
+    <Header />
+  </div>
+)
+}
+
+export default Home
