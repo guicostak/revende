ac0654e2e53ee956af9ee0c5eeb66017fa79cc76
guicostak <103201399+guicostak@users.noreply.github.com>
2023-04-30 19:23:24 -0300

    .

--- src/main/resources/componentesCadIngressos/Card/index.jsx
+++ src/main/resources/componentesCadIngressos/Card/index.jsx
@@ -7,11 +7,54 @@ import standUp from '../../img/vetores/categorias/standup.png'
 import palestras from '../../img/vetores/categorias/palestras.png'
 import infantis from '../../img/vetores/categorias/infantis.png'
 import outros from '../../img/vetores/categorias/outros.png'
+import Textfield from './Textfield'
+import Textarea from './Textarea'
+import camera from '../../img/vetores/camera.png'
+import remove from '../../img/vetores/remove.png'
+
 
 const Card = () => {
   const [animacao, setAnimacao] = useState('none')
   const [categoria, setCategoria] = useState('')
   const [outrosCampos, setOutrosCampos] = useState('none')
+  const [titulo, setTitulo] = useState('flex')
+  const [segundoTitulo, setSegundoTitulo] = useState('none')
+
+  const [showSelecionado, setShowSelecionado] = useState(false)
+  const [espetaculoSelecionado, setEspetaculoSelecionado] = useState(false)
+  const [esporteSelecionado, setEsporteSelecionado] = useState(false)
+  const [standupSelecionado, setStandupSelecionado] = useState(false)
+  const [palestraSelecionado, setPalestraSelecionado] = useState(false)
+  const [infantilSelecionado, setInfantilSelecionado] = useState(false)
+  const [outrosSelecionado, setOutrosSelecionado] = useState(false)
+
+  const [inputNome, setInputNome] = useState()
+  const [inputData, setInputData] = useState()
+  const [inputPreco, setInputPreco] = useState()
+  const [inputDescricao, setInputDescricao] = useState()
+  const [inputTelefone, setInputTelefone] = useState()
+  const [tipoIngresso, setTipoIngresso] = useState()
+  const [image, setImage] = useState('');
+  const [removeStatus, setRemoveStatus] = useState('none');
+
+  
+
+  function handleImageRemove() {
+    setImage("");
+    setRemoveStatus("none");
+    document.getElementById("imagem").value = null;
+  }
+
+  const handleImageChange = e => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+        setRemoveStatus('flex')
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,51 +62,201 @@ const Card = () => {
     }, 500)
   }, [])
 
-  function selecionaCategoria(e) {
+  function selecionaCategoria(e, classe) {
     setCategoria(e)
     setOutrosCampos('flex')
+    setTitulo('none')
+    setSegundoTitulo('flex')
+
+    setShowSelecionado(classe === 'showSelecionado' ? 'selecionado' : '')
+    setEspetaculoSelecionado(
+      classe === 'espetaculoSelecionado' ? 'selecionado' : ''
+    )
+    setEsporteSelecionado(classe === 'esporteSelecionado' ? 'selecionado' : '')
+    setStandupSelecionado(classe === 'standupSelecionado' ? 'selecionado' : '')
+    setPalestraSelecionado(
+      classe === 'palestraSelecionado' ? 'selecionado' : ''
+    )
+    setInfantilSelecionado(
+      classe === 'infantilSelecionado' ? 'selecionado' : ''
+    )
+    setOutrosSelecionado(classe === 'outrosSelecionado' ? 'selecionado' : '')
+  }
+
+  const cadastraIngresso = () => {
+
   }
 
   return (
-    <section className="cadIngressos-card">
-      <h1>Qual a categoria do ingresso anunciado?</h1>
+    <form onSubmit={cadastraIngresso} className="cadIngressos-card">
+      <h1 style={{ display: titulo }}>
+        Qual a categoria do ingresso anunciado?
+      </h1>
+      <h1 style={{ display: segundoTitulo }}>
+        Preencha algumas informações sobre o ingresso
+      </h1>
       <div className="card" style={{ display: animacao }}>
         <div className="categorias">
           <h2>Categorias*</h2>
           <ul>
-            <li onClick={() => selecionaCategoria('Shows e festas')}>
+            <li
+              onClick={() =>
+                selecionaCategoria('Shows e festas', 'showSelecionado')
+              }
+              className={showSelecionado}
+            >
               <img src={show} />
               Shows e festas <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Espetáculo e teatro')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria(
+                  'Espetáculo e teatro',
+                  'espetaculoSelecionado'
+                )
+              }
+              className={espetaculoSelecionado}
+            >
               <img src={teatro} />
               Espetáculo e teatro <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Eventos esportivos')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria('Eventos esportivos', 'esporteSelecionado')
+              }
+              className={esporteSelecionado}
+            >
               <img src={esportes} />
               Eventos esportivos <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Stand Up e comédia')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria('Stand Up e comédia', 'standupSelecionado')
+              }
+              className={standupSelecionado}
+            >
               <img src={standUp} />
               Stand Up e comédia <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Palestras e seminários')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria(
+                  'Palestras e seminários',
+                  'palestraSelecionado'
+                )
+              }
+              className={palestraSelecionado}
+            >
               <img src={palestras} />
               Palestras e seminários <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Eventos infantis')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria('Eventos infantis', 'infantilSelecionado')
+              }
+              className={infantilSelecionado}
+            >
               <img src={infantis} />
               Eventos infantis <div className="seta">&gt;</div>
             </li>
-            <li onClick={() => selecionaCategoria('Outras categorias')}>
+
+            <li
+              onClick={() =>
+                selecionaCategoria('Outras categorias', 'outrosSelecionado')
+              }
+              className={outrosSelecionado}
+            >
               <img src={outros} />
               Outras categorias <div className="seta">&gt;</div>
             </li>
           </ul>
         </div>
-        <div className="outros-campos" style={{ display: outrosCampos }}></div>
+        <div className="outros-campos" style={{ display: outrosCampos }}>
+          <div className="row">
+            <Textfield
+              label="Titulo*"
+              id="name"
+              type="text"
+              name="name"
+              max="30"
+              value={inputNome}
+              aoAlterado={value => setInputNome(value)}
+            />
+
+            <Textfield
+              label="Data*"
+              id="data"
+              type="date"
+              name="data"
+              value={inputData}
+              aoAlterado={value => setInputData(value)}
+            />
+
+            <Textfield
+              label="Preço(R$)*"
+              id="preco"
+              type="text"
+              name="preco"
+              max="12"
+              value={inputPreco}
+              aoAlterado={value => setInputPreco(value)}
+            />
+          </div>
+          <div className="row">
+            <Textarea
+              label="Descrição*"
+              id="descricao"
+              type="text"
+              name="descricao"
+              value={inputDescricao}
+              max="180"
+              aoAlterado={value => setInputDescricao(value)}
+            />
+
+          <div className='textfield' id='tipo'>
+          <label>Tipo de ingresso (f/d)*</label>
+          <select>
+            <option disabled selected>Selecione uma opção</option>
+            <option value={value =>setTipoIngresso('Físico')}>Físico</option>
+            <option value={value => setTipoIngresso('Digital')}>Digital</option>
+            <option value={value => setTipoIngresso('Ambos')}>Ambos</option>
+          </select>
+          <Textfield
+              label="Telefone*"
+              id="telefone"
+              type="text"
+              name="telefone"
+              max="15"
+              value={inputTelefone}
+              aoAlterado={value => setInputTelefone(value)}
+            />
+         </div>
+
+            
+          </div>
+          <div className='row'>
+            <div className='textfield' id="campoImagem">
+                <div className="upload-btn-wrapper">
+                  <img src={camera} className="btn"/>
+                  <input id="imagem" type="file" onChange={handleImageChange} />
+                </div>
+                {image && <img id="imagemCarregada" src={image} alt="Imagem carregada" />}
+                <img style={{display: removeStatus}} src={remove} className='remover-imagem' onClick={handleImageRemove}/>
+              </div>
+              <div className='textfield'>
+              <p>*Preencha todos os campos, e confira os dados.</p>
+              <button id='submitIngresso'>anunciar</button>
+              </div>
+            </div>
+
+        </div>
       </div>
-    </section>
+    </form>
   )
 }
 
--- src/main/resources/componentesHome/Ingressos/index.jsx
+++ src/main/resources/componentesHome/Ingressos/index.jsx
@@ -1,70 +1,224 @@
-import './Ingressos.scss';
-import React, { useEffect, useState } from 'react';
-import { useSwiper, Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
+import './Ingressos.scss'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSwiper, Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import Ingresso from './Ingresso';
-import axios from 'axios';
+import Ingresso from './Ingresso'
+import axios from 'axios'
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([Navigation])
+
+
 
 const Ingressos = () => {
-  const [vetorIngressos, setVetorIngressos] = useState([]);
+  const [vetorIngressos, setVetorIngressos] = useState([
+    {
+      img: 1,
+      titulo: 'Ingresso 1',
+      data: '28/04',
+      preco: 'R$' + 10,
+      tipo: 'fisico'
+    },
+    {
+      img: 2,
+      titulo: 'Ingresso 2',
+      data: '28/04',
+      preco: 'R$' + 15,
+      tipo: 'digital'
+    },
+    {
+      img: 3,
+      titulo: 'Ingresso 3',
+      data: '28/04',
+      preco: 'R$' + 20,
+      tipo: 'fisico'
+    },
+    {
+      img: 4,
+      titulo: 'Ingresso 4',
+      data: '29/04',
+      preco: 'R$' + 25,
+      tipo: 'digital'
+    },
+    {
+      img: 5,
+      titulo: 'Ingresso 5',
+      data: '30/04',
+      preco: 'R$' + 30,
+      tipo: 'fisico'
+    },
+    {
+      img: 6,
+      titulo: 'Ingresso 6',
+      data: '01/05',
+      preco: 'R$' + 35,
+      tipo: 'digital'
+    },
+    {
+      img: 7,
+      titulo: 'Ingresso 7',
+      data: '02/05',
+      preco: 'R$' + 40,
+      tipo: 'fisico'
+    },
+    {
+      img: 8,
+      titulo: 'Ingresso 8',
+      data: '03/05',
+      preco: 'R$' + 45,
+      tipo: 'digital'
+    },
+    {
+      img: 9,
+      titulo: 'Ingresso 9',
+      data: '04/05',
+      preco: 'R$' + 50,
+      tipo: 'fisico'
+    },
+    {
+      img: 10,
+      titulo: 'Ingresso 10',
+      data: '05/05',
+      preco: 'R$' + 55,
+      tipo: 'digital'
+    }
+  ])
 
   useEffect(() => {
     axios
       .get('http://localhost:8080/inicio/ingressos')
-      .then((response) => setVetorIngressos(response.data))
-      .catch((error) => console.log(error));
-  }, []);
+      .then(response => setVetorIngressos(response.data))
+      .catch(error => console.log(error))
+  }, [])
 
-  const swiper = useSwiper();
+  const swiper = useSwiper()
+  const sliderRef = useRef(null)
 
-  const dezMaisRecentes = vetorIngressos.slice(-10).reverse();
+  const dezMaisRecentes = vetorIngressos.slice(-10).reverse()
 
   return (
     <div className="ingressos">
+      <h1>Anuncios recentes</h1>
       {vetorIngressos.length > 0 ? (
-        <div className="listaIngressos">
-          <button className="navButton" onClick={() => swiper.slidePrev()}>
+        <div className="listaIngressos" ref={sliderRef}>
+          <button id='prev'className="navButton" onClick={() => swiper.slidePrev()}>
             &lt;
           </button>
-          <Swiper>
+          <Swiper
+            modules={[Navigation]}
+            loop
+            onSwiper={swiper => swiper.update()}
+            navigation={{
+              prevEl: '#prev',
+              nextEl: '#next'
+            }}>
             <SwiperSlide>
-              <Ingresso
-                imgIngresso={dezMaisRecentes[0].img}
-                tituloIngresso={dezMaisRecentes[0].titulo}
-                dataIngresso={dezMaisRecentes[0].data}
-                precoIngresso={dezMaisRecentes[0].preco}
-                tipoIngresso={dezMaisRecentes[0].tipo}
-              />
+              <div className="parIngressos">
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[0].img}
+                  tituloIngresso={dezMaisRecentes[0].titulo}
+                  dataIngresso={dezMaisRecentes[0].data}
+                  precoIngresso={dezMaisRecentes[0].preco}
+                  tipoIngresso={dezMaisRecentes[0].tipo}
+                />
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[1].img}
+                  tituloIngresso={dezMaisRecentes[1].titulo}
+                  dataIngresso={dezMaisRecentes[1].data}
+                  precoIngresso={dezMaisRecentes[1].preco}
+                  tipoIngresso={dezMaisRecentes[1].tipo}
+                />
+              </div>
             </SwiperSlide>
-
             <SwiperSlide>
-              <Ingresso
-                imgIngresso={dezMaisRecentes[1].img}
-                tituloIngresso={dezMaisRecentes[1].titulo}
-                dataIngresso={dezMaisRecentes[1].data}
-                precoIngresso={dezMaisRecentes[1].preco}
-                tipoIngresso={dezMaisRecentes[1].tipo}
-              />
+              <div className="parIngressos">
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[2].img}
+                  tituloIngresso={dezMaisRecentes[2].titulo}
+                  dataIngresso={dezMaisRecentes[2].data}
+                  precoIngresso={dezMaisRecentes[2].preco}
+                  tipoIngresso={dezMaisRecentes[2].tipo}
+                />
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[3].img}
+                  tituloIngresso={dezMaisRecentes[3].titulo}
+                  dataIngresso={dezMaisRecentes[3].data}
+                  precoIngresso={dezMaisRecentes[3].preco}
+                  tipoIngresso={dezMaisRecentes[3].tipo}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="parIngressos">
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[4].img}
+                  tituloIngresso={dezMaisRecentes[4].titulo}
+                  dataIngresso={dezMaisRecentes[4].data}
+                  precoIngresso={dezMaisRecentes[4].preco}
+                  tipoIngresso={dezMaisRecentes[4].tipo}
+                />
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[5].img}
+                  tituloIngresso={dezMaisRecentes[5].titulo}
+                  dataIngresso={dezMaisRecentes[5].data}
+                  precoIngresso={dezMaisRecentes[5].preco}
+                  tipoIngresso={dezMaisRecentes[5].tipo}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="parIngressos">
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[6].img}
+                  tituloIngresso={dezMaisRecentes[6].titulo}
+                  dataIngresso={dezMaisRecentes[6].data}
+                  precoIngresso={dezMaisRecentes[6].preco}
+                  tipoIngresso={dezMaisRecentes[6].tipo}
+                />
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[7].img}
+                  tituloIngresso={dezMaisRecentes[7].titulo}
+                  dataIngresso={dezMaisRecentes[7].data}
+                  precoIngresso={dezMaisRecentes[7].preco}
+                  tipoIngresso={dezMaisRecentes[7].tipo}
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="parIngressos">
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[8].img}
+                  tituloIngresso={dezMaisRecentes[8].titulo}
+                  dataIngresso={dezMaisRecentes[8].data}
+                  precoIngresso={dezMaisRecentes[8].preco}
+                  tipoIngresso={dezMaisRecentes[8].tipo}
+                />
+                <Ingresso
+                  imgIngresso={dezMaisRecentes[9].img}
+                  tituloIngresso={dezMaisRecentes[9].titulo}
+                  dataIngresso={dezMaisRecentes[9].data}
+                  precoIngresso={dezMaisRecentes[9].preco}
+                  tipoIngresso={dezMaisRecentes[9].tipo}
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
 
-          <button className="navButton" onClick={() => swiper.slideNext()}>
+          <button id='next' className="navButton" onClick={() => swiper.slideNext()}>
             &gt;
           </button>
         </div>
       ) : (
         <div className="ingressos">
-          <div className='sem-ingressos'>
-            <p>Não existem ingressos disponíveis.</p>
+          <div className="sem-ingressos">
+            <h1></h1>
+            <p>Não existem ingressos disponíveis nessa sessão.</p>
           </div>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Ingressos;
+export default Ingressos
