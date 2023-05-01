e8cbf558804d3605cf8b332e86c66a3515f7f2d5
guicostak <103201399+guicostak@users.noreply.github.com>
2023-05-01 17:38:26 -0300

    .

--- src/main/resources/componentesCadIngressos/Header/index.jsx
+++ src/main/resources/componentesCadIngressos/Header/index.jsx
@@ -1,10 +1,11 @@
 import logo from '../../img/logos/logo.png'
 import './Header.scss'
+import { Link } from 'react-router-dom'
 
 const Header = () =>{
   return(
       <header className='cadIngresso-header'>
-        <img src={logo}/>
+        <Link to="/"><img src={logo}/></Link>
         <p>Olá, Guilherme</p>
       </header>
   )
